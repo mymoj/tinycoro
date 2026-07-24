f17f598cd8173201c9020b27f96497869ca0b729
moj <81232807+mymoj@users.noreply.github.com>
2026-07-24 10:51:49 +0800

    Implement context initialization and deinitialization
    Initialize and deinitialize context with engine methods.

--- src/context.cpp
+++ src/context.cpp
@@ -11,11 +11,15 @@ context::context() noexcept
 auto context::init() noexcept -> void
 {
     // TODO[lab2b]: Add you codes
+    linfo.ctx = this;
+    m_engine.init();
 }
 
 auto context::deinit() noexcept -> void
 {
     // TODO[lab2b]: Add you codes
+    linfo.ctx = nullptr;
+    m_engine.deinit();
 }
 
 auto context::start() noexcept -> void
@@ -38,11 +42,13 @@ auto context::notify_stop() noexcept -> void
 auto context::submit_task(std::coroutine_handle<> handle) noexcept -> void
 {
     // TODO[lab2b]: Add you codes
+    m_engine.submit_task(handle);
 }
 
 auto context::register_wait(int register_cnt) noexcept -> void
 {
     // TODO[lab2b]: Add you codes
+    m_num_wait_task.fetch_add(size_t(register_cnt), memory_order_acq_rel);
 }
 
 auto context::unregister_wait(int register_cnt) noexcept -> void
